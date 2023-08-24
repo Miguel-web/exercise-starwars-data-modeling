4b26d03cb015d8f170bbe84ced2e3ada08e9dd20
Miguel Gil F <67928892+Miguel-web@users.noreply.github.com>
2023-08-24 20:29:56 +0000

    ready clases, atributos,relations

--- src/models.py
+++ src/models.py
@@ -7,26 +7,53 @@ from eralchemy2 import render_er
 
 Base = declarative_base()
 
-class Person(Base):
-    __tablename__ = 'person'
-    # Here we define columns for the table person
-    # Notice that each column is also a normal Python instance attribute.
+class Usuario(Base):
+    __tablename__ = 'usuario'
     id = Column(Integer, primary_key=True)
-    name = Column(String(250), nullable=False)
+    nombre = Column(String(250), nullable=False)
+    apellido = Column(String(250), nullable=False)
+    email = Column(String(250), nullable=False)
+    password = Column(String(250), nullable=False)
+    fecha_de_subscripcion = Column(String(250), nullable=False)
+    usuario = Column(String(250), nullable=False)
+    favoritos = relationship("Favorito", back_populates = "usuario")
 
-class Address(Base):
-    __tablename__ = 'address'
-    # Here we define columns for the table address.
-    # Notice that each column is also a normal Python instance attribute.
+class Personaje(Base):
+    __tablename__ = 'personaje'
     id = Column(Integer, primary_key=True)
-    street_name = Column(String(250))
-    street_number = Column(String(250))
-    post_code = Column(String(250), nullable=False)
-    person_id = Column(Integer, ForeignKey('person.id'))
-    person = relationship(Person)
+    altura = Column(String(250), nullable=False)
+    genero = Column(String(250), nullable=False)
+    color_cabello = Column(String(250), nullable=False)
+    color_ojos = Column(String(250), nullable=False)
+    edad = Column(String(250), nullable=False)
+    nombre = Column(String(250), nullable=False)
+    favoritos = relationship("Favorito", back_populates = "personaje")
 
     def to_dict(self):
         return {}
+
+class Planeta(Base):
+    __tablename__ = 'planeta'
+    id = Column(Integer, primary_key=True)
+    diametro = Column(String(250), nullable=False)
+    periodo_rotacion = Column(String(250), nullable=False)
+    terreno = Column(String(250), nullable=False)
+    clima = Column(String(250), nullable=False)
+    nombre = Column(String(250), nullable=False)
+    descripcion = Column(String(250), nullable=False)
+    favoritos = relationship("Favorito", back_populates = "planeta")
+
+class Favorito(Base):
+    __tablename__ = 'favorito'
+    id = Column(Integer, primary_key=True)
+    usuario_id = Column(Integer, ForeignKey("usuarios.id"))
+    personaje_id = Column(Integer, ForeignKey("personajes.id"))
+    planeta_id = Column(Integer, ForeignKey("planetas.id"))
+    usuario = relationship("Usuario", back_populates = "favoritos")
+    planeta = relationship("Planeta", back_populates = "favoritos")
+    personaje = relationship("Personaje", back_populates = "favoritos")
+    
+
 
 ## Draw from SQLAlchemy base
 render_er(Base, 'diagram.png')
